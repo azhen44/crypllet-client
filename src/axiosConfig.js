ff80cf94dcd38d41093cf6c3800601e7d3582aee
andy zhen <andyzhen44@gmail.com>
2022-05-23 23:32:13 -0700

    netlify config

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
- baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:3000",
+ baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000",
 });
+
 
 export default axiosConfig;
